cc745bf5c9f51da70a7c694794367b3536d353ef
alanleonel.rd17 <alanleonel.rd17@gmail.com>
2024-07-26 05:21:16 -0300

    "dia2"

---
+++ react_fronted_integrador/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {PI} from "./funciones"
+
+console.log (PI)
+
+const App = () => {
+return(
+    <div>
+        <h1>Pagina Principal</h1>
+        <QuienesSomos/>
+    </div>
+    )
+}
+
+const QuienesSomos = () => {
+    return(
+    <>
+        <div>
+            <h2>Porque Literatura Ambulante...</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, accusantium incidunt quis sint eum natus quam tempore itaque dolore tenetur commodi nemo tempora cupiditate libero placeat magnam vel, architecto voluptatibus?</p>
+            <button>Me gusta</button>
+            <Span>Alan Ruiz Diaz, Estudiante.</Span>
+        </div>
+        <div>
+            <button>Continuar</button>
+        </div> 
+    </>
+    )
+}
+
+
+export default App
---
+++ react_fronted_integrador/src/funciones.js
@@ -0,0 +1,7 @@
+const PI = 3.14
+
+
+
+
+
+export {PI}
