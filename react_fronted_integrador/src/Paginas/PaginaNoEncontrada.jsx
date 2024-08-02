224ae8d6e73d1f1b577837fc1fd0f6dacc417fbc
alanleonel.rd17 <alanleonel.rd17@gmail.com>
2024-08-02 17:14:20 -0300

    "otros-avances"

---
+++ react_fronted_integrador/src/Paginas/PaginaNoEncontrada.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PaginaNoEncontrada = () => {
+    return (
+        <div>
+            <h2>404 - No Encontrado</h2>
+            <p>La página que estás buscando no existe.</p>
+        </div>
+    )
+}
+
+export default PaginaNoEncontrada;
---
+++ react_fronted_integrador/src/Paginas/PaginaOpiniones.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PaginaOpiniones = () => {
+    return (
+        <div>
+            <h2>Página de Opiniones</h2>
+            {/* Falta funcionalidades aca con respecto a las opiniones */}
+        </div>
+    )
+}
+
+export default PaginaOpiniones
